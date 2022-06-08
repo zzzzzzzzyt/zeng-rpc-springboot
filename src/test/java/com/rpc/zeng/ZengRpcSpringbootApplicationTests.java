c561f4db5ffaab6d5b7a82c34481606eecbeaa71
asus <836585692@qq.com>
2022-06-08 09:13:19 +0800

    2022.6.8 zengRpc springboot版

---
+++ src/test/java/com/rpc/zeng/ZengRpcSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpc.zeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZengRpcSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
